40b971452a8f6c9201d3c0756f3b9fc1d5aa1cb3
AnnareddyCharitha <m95017277@gmail.com>
2026-03-09 14:55:56 +0530

    updated

---
+++ backend/controller/taskController.js
@@ -0,0 +1,12 @@
+exports.createTask=async(req,res)=>{
+    try{
+        const task=await Task.create=({
+            title:req.body.description,
+            status:req.body.status,
+            user:req.user.id
+        })
+        res.status(201).json({msg:"task created"},task)
+    }catch(error){
+        console.log(error)
+    }
+}
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const {createTask}=require('../controller/taskController')
 const {protect}=require('../middleware/authmiddleware')
 router.post('/create',protect,createTask)
+
 module.exports = router;
